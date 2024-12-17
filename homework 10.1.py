9268ffbdbd172ba50315f349817fa559b9ede6fe
Vsevolod <sevach92@yandex.ru>
2024-12-18 00:16:27 +0400

    Add files via upload

---
+++ homework 10.1.py
@@ -0,0 +1,35 @@
+import time
+from time import sleep
+from threading import Thread
+
+def wite_words(word_count, file_name):
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for n in range(1, word_count + 1):
+            file.write(f'Какое-то слово №{n}\n')
+            sleep(0.1)
+    print(f'Завершилась запись в файл{file_name}')
+
+time_start = time.time()
+wite_words(10, 'example1.txt')
+wite_words(30, 'example2.txt')
+wite_words(200, 'example3.txt')
+wite_words(100, 'example4.txt')
+time_end = time.time()
+print(f'Время работы потоков {time_end - time_start} секунд')
+
+time_start = time.time()
+thread1 = Thread(target=wite_words, args=(10, 'example5.txt'))
+thread2 = Thread(target=wite_words, args=(30, 'example6.txt'))
+thread3 = Thread(target=wite_words, args=(200, 'example7.txt'))
+thread4 = Thread(target=wite_words, args=(100, 'example8.txt'))
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+
+thread1.join()
+thread2.join()
+thread3.join()
+thread4.join()
+time_end= time.time()
+print(f'Время работы потоков {time_end - time_start} секунд')
